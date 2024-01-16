e8b54a4ef3fc6b2c79e1854318a8211df3dac079
AndrewLondon <andrewmart@mail.com>
2024-01-16 17:23:27 +0000

    Updated

--- iterations/odd_occurences_in_array.go
+++ iterations/odd_occurences_in_array.go
@@ -1,10 +1,10 @@
 package main
 
 /*
-A non-empty array A consisting of N integers is given. 
-The array contains an odd number of elements, 
-and each element of the array can be paired with 
-another element that has the same value, 
+A non-empty array A consisting of N integers is given.
+The array contains an odd number of elements,
+and each element of the array can be paired with
+another element that has the same value,
 except for one element that is left unpaired.
 
 For example, in array A such that:
@@ -20,8 +20,8 @@ Write a function:
 
 func Solution(A []int) int
 
-that, given an array A consisting of N integers 
-fulfilling the above conditions, returns the value 
+that, given an array A consisting of N integers
+fulfilling the above conditions, returns the value
 of the unpaired element.
 
 For example, given array A such that:
@@ -38,4 +38,19 @@ each element of array A is an integer within the range [1..1,000,000,000];
 all but one of the values in A occur an even number of times.
 */
 
-func 
+import "fmt"
+
+func Solution(A []int) int {
+	result := 0
+
+	for _, num := range A {
+		result ^= num
+	}
+
+	return result
+}
+
+func main() {
+	A := []int{9, 3, 9, 3, 9, 7, 9}
+	fmt.Println(Solution(A)) // Output: 7
+}
